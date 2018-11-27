0d0029fbcfc239f5411cb63b66ed7960bd2d876d
keronei <keronei95@gmail.com>
2018-11-27 14:10:31 +0300

    This is the endpoint for creating new red-flag entry [#162232965]

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,8 +4,8 @@ from flask_restful import Api, Resource
 
 from flask import Blueprint
 
-version_one = Blueprint('api_v1',__name__, url_prefix = '/api/v1/')
+version_one = Blueprint('api_v1',__name__, url_prefix = '/api/v1/red-flags/')
 
 api = Api(version_one)
 
-api.add_resource(iReporter,'/red-flags')
+api.add_resource(iReporter,'/')

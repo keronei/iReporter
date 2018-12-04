79031ffe3511cc2fd625ce93af48f45ec3b25b12
keronei <keronei95@gmail.com>
2018-12-04 22:00:33 +0300

    fixed vcs versioning issues

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,5 @@ def launcher():
     app = Flask(__name__)
     app.config.from_object(app_config['development'])
     app.register_blueprint(v1)
-<<<<<<< HEAD
     return app
-=======
-    return app
->>>>>>> 3b255b5c37a95a6d2c78cf52dd281a686ea26a0e
+

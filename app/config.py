032eff4d43ae2911fada32ea55616300c69a9851
keronei <keronei95@gmail.com>
2018-12-04 21:45:07 +0300

    Reorganized VCS to follow version releases

---
+++ app/config.py
@@ -0,0 +1,35 @@
+import os
+class OverallConfig(object):
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    POSTGRES = {
+    'user':'keronei',
+    'pw':'',
+    'db':'model',
+    'host':'localhost',
+    'port':'5432'
+    }
+    SQLALCHEMY_DATABASE_URI =  'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s'%POSTGRES
+class Development(OverallConfig):
+    """
+    dev environ configuration
+    """
+    DEBUG = True
+    TESTING = True
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+class Production(OverallConfig):
+    """
+    pro env config
+    """
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    
+    
+app_config = {
+    'development': Development,
+    'production': Production,
+}
+    

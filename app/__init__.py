93968b97bc486539731dbce849c7617ad05b0447
keronei <keronei95@gmail.com>
2018-12-05 10:45:18 +0300

    Fixed versioning

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
 
 from flask import Flask, Blueprint
 from .api.v1 import version_one as v1
-from .api.v2 import version_two as v2
 from .config import app_config
-from .api.v2.models import db
 
 def launcher(config=None):
     app = Flask(__name__)
@@ -12,5 +10,4 @@ def launcher(config=None):
     app.config.from_object(config)
     db.init_app(app)
     app.register_blueprint(v1)
-    app.register_blueprint(v2)
     return app

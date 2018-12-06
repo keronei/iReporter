d088280fcef431ecfa4e6babc200cca9b2a4c49d
keronei <keronei95@gmail.com>
2018-12-06 19:39:07 +0300

    Implemented validation to user input for creating new account

--- app/api/v2/views/user_view.py
+++ app/api/v2/views/user_view.py
@@ -3,6 +3,7 @@ This file receives and serves requests as api endpoints.
 """
 from flask import request, jsonify, Response
 from flask_restful import Resource
+from validate_email import validate_email
 import json
 from ..models.user_model import UserModel
 
@@ -17,12 +18,36 @@ class UserView(Resource):
                
     def post(self):
         """Sets new data to database(Dict)"""
+        received_data = request.get_json()
+        count = 0
+        for entry in received_data:
+            count += 1
+        if count < 6:
+            return self.custom_response({'status': 200, 'data': 'Less params.'}, 200)
+        if self.validator(received_data):
+            result = self.model.add_user(received_data)
+            return Helper.custom_response({"data": result}, 200)
+        return self.custom_response({'status': 200, 'data': 'Your credentials were invalid.'}, 200)
+     
         
-        result = self.model.add_user(request.get_json())
+    def validator(self, json_data):
+        """Check all the values in json and assign back to error,
+        Finally, the result would remain in error as bool"""
+        formated = json.dumps(json_data)
+        string_formatted = json.loads(formated)
+
+        error_firstname = isinstance(string_formatted["firstname"], str)
+        error_secondname = isinstance(string_formatted["secondname"], str)
+        error_phone = isinstance(string_formatted["phoneNumber"], str)
+        error_username = isinstance(string_formatted["username"], str)        
+        is_valid = validate_email('example@example.com')
         
-        return Helper.custom_response({"data": result}, 200)
+        if False in {error_firstname, error_secondname, error_phone, error_username, is_valid} :
+           return False
+        return True
         
 class Helper():
+    
     def custom_response(res, status_code):
         """
         Custom Response Function

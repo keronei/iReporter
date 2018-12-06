62323679dd4906dc5bdd17c7aa940960a26c0e21
keronei <keronei95@gmail.com>
2018-12-06 10:49:40 +0300

    removed helper methods as were unused

--- app/api/v2/views/user_view.py
+++ app/api/v2/views/user_view.py
@@ -13,16 +13,7 @@ class UserView(Resource):
     """
     def __init__(self):
         """Instantiate the db as the database"""
-        self.database = "ker"
         self.model = UserModel()
-    def get(self, identifier=None):
-        """
-        Serves the GET request, But checks if any param is given in order to determine
-        if its a single entry request or all
-        """
-        self.add_user(request.get_json()) 
-        return incidence.dump(raw_single)
-        #return result
                
     def post(self):
         """Sets new data to database(Dict)"""

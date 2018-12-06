ec2e2d3e8810449a00867db6412be9595acea224
keronei <keronei95@gmail.com>
2018-12-06 20:43:21 +0300

    Wrongly branched file fix

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,6 +1,6 @@
 
 from .models.user_model import UserModel
-from .views.user_view import UserView, UserLogin
+from .views.user_view import UserView
 from flask_restful import Api, Resource
 
 from flask import Blueprint
@@ -11,7 +11,7 @@ version_two = Blueprint('api_v2',__name__)
 api = Api(version_two)
 
 api.add_resource(UserView,'/','/api/v2/auth/signup')
-api.add_resource(UserLogin,'/','/api/v2/auth/signin')
+
 
 
 

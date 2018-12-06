f551666924c0cb01ac197c555a7dd972cca6c65e
keronei <keronei95@gmail.com>
2018-12-06 10:39:47 +0300

    Added database config and /auth/ routing in blueprint

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
 import os
+import psycopg2
 class OverallConfig(object):
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     POSTGRES = {
     'user':'keronei',
@@ -8,7 +11,7 @@ class OverallConfig(object):
     'host':'localhost',
     'port':'5432'
     }
-    SQLALCHEMY_DATABASE_URI =  'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s'%POSTGRES
+    
 class Development(OverallConfig):
     """
     dev environ configuration
@@ -16,20 +19,66 @@ class Development(OverallConfig):
     DEBUG = True
     TESTING = True
     
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
 class Production(OverallConfig):
     """
     pro env config
     """
     DEBUG = False
-    TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    
+    TESTING = False    
     
 app_config = {
     'development': Development,
     'production': Production,
 }
-
+class DataBase():
+    def __init__(self):
+        self.db = psycopg2.connect(host="localhost", database="users", user="keronei", password="")
+    def create_tables(self):
+        #Create the tables, but first check if they exist.
+        create = ("""
+            CREATE TABLE IF NOT EXISTS  users (
+                    id SERIAL PRIMARY KEY,
+                    firstname VARCHAR(50) NOT NULL,
+                    lastname VARCHAR(50) NOT NULL,
+                    othernames VARCHAR(50),
+                    email VARCHAR(50) NOT NULL,
+                    username VARCHAR(50) NOT NULL,
+                    registered TIMESTAMP,
+                    phoneNumber VARCHAR(15) NOT NULL,
+                    password VARCHAR(200) NOT NULL,
+                    isAdmin BOOLEAN
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS  incident (
+                    incident_id SERIAL PRIMARY KEY,
+                    createdOn VARCHAR(50) NOT NULL,
+                    createdBy INTEGER NOT NULL,
+                    type VARCHAR(50) NOT NULL,
+                    location VARCHAR(50),
+                    status VARCHAR(50) NOT NULL,
+                    images VARCHAR(250) NOT NULL,
+                    videos VARCHAR(250) NOT NULL,
+                    comment VARCHAR(250) NOT NULL
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS incidence_users (
+                    id INTEGER NOT NULL,
+                    incident_id INTEGER NOT NULL,
+                    PRIMARY KEY (id , incident_id),
+                    FOREIGN KEY (incident_id)
+                        REFERENCES users (id)
+                        ON UPDATE CASCADE ON DELETE CASCADE,
+                    FOREIGN KEY (incident_id)
+                        REFERENCES incident (incident_id)
+                        ON UPDATE CASCADE ON DELETE CASCADE
+            )
+            """)
+        cursor = self.db.cursor()
+        for tables in create:
+            cursor.execute(tables)
+        cursor.close()
+        self.db.commit()
+        
